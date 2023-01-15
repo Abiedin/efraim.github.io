40c0687f4cd29ace59eb276884170f432bac2ec4
Bedin Andriy <bulgar198605@gmail.com>
2023-01-15 20:07:32 +0200

    2

---
+++ src/components/books-tora/Shmot.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../tora.scss';
+
+const Shmot = () => {
+  return (
+    <>
+      <div className="book__fals">
+        <div className="book__fals-one">
+          SHMOT
+        </div>          
+      </div>
+    </>
+  );
+};
+
+export default  Shmot;
